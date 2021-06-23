1c779a783c007d50b985ffd429738abfdfb9ae1a
Ben Hall <benjamhall@gmail.com>
2021-06-22 22:28:05 -0500

    added conditional statements on secrets.router

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require(`../modules/authentication-middleware`)
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
+  
   // what is the value of req.user????
-  console.log('req.user:', req.user);
+  if(req.isAuthenticated()) {
+    console.log('/secrets GET Route');
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('req.user:', req.user);
+
+    if(req.user.clearance_level >= 13) {
+      pool
+        .query(`SELECT * FROM "secret" `)
+        .then((results) => res.send(results.rows))
+        .catch((error) => {
+          console.log('Error making SELECT for secrets:', error);
+          res.sendStatus(500);
+        });
+      }
+    // if(req.user.clearance_level >=)
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`SELECT * FROM "secret" `)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  
+  } else {
+      res.sendStatus(403);
+  }
 });
 
 module.exports = router;
